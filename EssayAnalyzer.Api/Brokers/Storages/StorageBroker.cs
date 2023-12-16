6964596157b1731d41b892878aa3d4a43713c26c
Ravshan9812 <anorboyev@gmail.com>
2023-12-16 17:43:57 +0500

    CODE RUB: fixed namespaces

--- EssayAnalyzer.Api/Brokers/Storages/StorageBroker.cs
+++ EssayAnalyzer.Api/Brokers/Storages/StorageBroker.cs
@@ -1,67 +1,65 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
+﻿using EFxceptions;
+using Microsoft.EntityFrameworkCore;
 
-namespace EssayAnalyzer.Api.Brokers.Storages
+namespace EssayAnalyzer.Api.Brokers.Storages;
+public partial class StorageBroker : EFxceptionsContext, IStorageBroker
 {
-    public partial class StorageBroker : EFxceptions.EFxceptionsContext, IStorageBroker
+    private readonly IConfiguration configuration;
+
+    public StorageBroker(IConfiguration configuration)
     {
-        private readonly IConfiguration configuration;
+        this.configuration = configuration;
+        this.Database.Migrate();
 
-        public StorageBroker(IConfiguration configuration)
-        {
-            this.configuration = configuration;
-            this.Database.Migrate();
+    }
 
-        }
+    public async ValueTask<T> InsertAsync<T>(T @object)
+    {
+        var broker = new StorageBroker(this.configuration);
 
-        public async ValueTask<T> InsertAsync<T>(T @object)
-        {
-            var broker = new StorageBroker(this.configuration);
+        broker.Entry(@object).State = EntityState.Added;
+        await broker.SaveChangesAsync();
 
-            broker.Entry(@object).State = EntityState.Added;
-            await broker.SaveChangesAsync();
+        return @object;
+    }
 
-            return @object;
-        }
+    public async ValueTask<T> SelectAsync<T>(params object[] objectIds) where T : class
+    {
+        var broker = new StorageBroker(this.configuration);
 
-        public async ValueTask<T> SelectAsync<T>(params object[] objectIds) where T : class
-        {
-            var broker = new StorageBroker(this.configuration);
+        return await broker.FindAsync<T>(objectIds);
+    }
 
-            return await broker.FindAsync<T>(objectIds);
-        }
+    public IQueryable<T> SelectAll<T>() where T : class
+    {
+        var broker = new StorageBroker(this.configuration);
 
-        public IQueryable<T> SelectAll<T>() where T : class
-        {
-            var broker = new StorageBroker(this.configuration);
+        return broker.Set<T>();
+    }
 
-            return broker.Set<T>();
-        }
+    public async ValueTask<T> UpdateAsync<T>(T @object)
+    {
+        var broker = new StorageBroker(this.configuration);
+        broker.Entry(@object).State = EntityState.Modified;
+        await broker.SaveChangesAsync();
 
-        public async ValueTask<T> UpdateAsync<T>(T @object)
-        {
-            var broker = new StorageBroker(this.configuration);
-            broker.Entry(@object).State = EntityState.Modified;
-            await broker.SaveChangesAsync();
+        return @object;
+    }
 
-            return @object;
-        }
+    public async ValueTask<T> DeleteAsync<T>(T @object)
+    {
+        var broker = new StorageBroker(this.configuration);
+        broker.Entry(@object).State = EntityState.Deleted;
+        await broker.SaveChangesAsync();
 
-        public async ValueTask<T> DeleteAsync<T>(T @object)
-        {
-            var broker = new StorageBroker(this.configuration);
-            broker.Entry(@object).State = EntityState.Deleted;
-            await broker.SaveChangesAsync();
+        return @object;
+    }
 
-            return @object;
-        }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string connectionString =
+            configuration.GetConnectionString("DefaultConnection");
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            string connectionString =
-                configuration.GetConnectionString("DefaultConnection");
-
-            optionsBuilder.UseNpgsql(connectionString);
-        }
+        optionsBuilder.UseNpgsql(connectionString);
     }
 }
