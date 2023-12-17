a0b079c37acb33c6649c9dc73c1083ce5eb407db
Jurabek <jorabeksindarov44@gmail.com>
2023-12-17 16:54:11 +0500

    Brokers are registred in Program.cs

--- EssayAnalyzer.Api/Program.cs
+++ EssayAnalyzer.Api/Program.cs
@@ -1,6 +1,12 @@
+using EssayAnalyzer.Api.Brokers.Loggings;
+using EssayAnalyzer.Api.Brokers.OpenAis;
 using EssayAnalyzer.Api.Brokers.Storages;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<IStorageBroker, StorageBroker>();
+builder.Services.AddTransient<ILoggingBroker, LoggingBroker>();
+builder.Services.AddTransient<IOpenAiBroker, OpenAiBroker>();
+
 var app = builder.Build();
 
 builder.Services.AddDbContext<StorageBroker>();
