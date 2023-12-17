8fa95edc97b6f3264bd2c95894bb5858443ea6dc
Azam Amonov <azamamonov555@gmail.com>
2023-12-17 16:56:22 +0500

    CODE RUB: Retrieve All Essays

--- EssayAnalyzer.Api/Services/Foundation/Essays/IEssayService.cs
+++ EssayAnalyzer.Api/Services/Foundation/Essays/IEssayService.cs
@@ -5,7 +5,7 @@ namespace EssayAnalyzer.Api.Services.Foundation.Essays;
 public partial interface IEssayService
 {
   public ValueTask<Essay> AddEssayAsync(Essay essay);
-  public ValueTask<IQueryable<Essay>> RetrieveEssaysAsync();
+  public IQueryable<Essay> RetrieveAllEssays();
   public ValueTask<Essay> RetrieveEssayByIdAsync(Guid id);
   public ValueTask<Essay> RemoveEssayByIdAsync(Guid id );
 }
