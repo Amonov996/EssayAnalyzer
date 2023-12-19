16f0b419d8a46e8d01490ebb513c4e69edb55077
Azam Amonov <azamamonov555@gmail.com>
2023-12-19 21:09:12 +0500

    POC: Add Delete Methods for Essay and Result

--- EssayAnalyzer.Api/Brokers/Storages/StorageBroker.Results.cs
+++ EssayAnalyzer.Api/Brokers/Storages/StorageBroker.Results.cs
@@ -7,13 +7,16 @@ namespace EssayAnalyzer.Api.Brokers.Storages
     {
         public DbSet<Result> Results { get; set; }
 
+        public IQueryable<Result> SelectAllResults() =>
+            SelectAll<Result>();
+        
         public async ValueTask<Result> InsertResultAsync(Result result) =>
             await InsertAsync(result);
 
         public async ValueTask<Result> SelectResultByIdAsync(Guid id) =>
             await SelectAsync<Result>(id);
-
-        public IQueryable<Result> SelectAllResults() =>
-            SelectAll<Result>();
+        
+        public async ValueTask<Result> DeleteEssayAsync(Result essay) => 
+            await DeleteAsync<Result>(essay);
     }
 }
