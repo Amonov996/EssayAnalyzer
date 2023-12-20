a878f5095a9d7fd6241735e99786b23641b251c8
Azam Amonov <azamamonov555@gmail.com>
2023-12-20 19:45:33 +0500

    CODE RUB: x

--- EssayAnalyzer.Api/Services/Foundation/EssayAnalyzes/EssayAnalysisService.cs
+++ EssayAnalyzer.Api/Services/Foundation/EssayAnalyzes/EssayAnalysisService.cs
@@ -2,56 +2,55 @@
 using EssayAnalyzer.Api.Brokers.OpenAis;
 using Standard.AI.OpenAI.Models.Services.Foundations.ChatCompletions;
 
-namespace EssayAnalyzer.Api.Services.Foundation.EssayAnalyzes
+namespace EssayAnalyzer.Api.Services.Foundation.EssayAnalyzes;
+
+public partial class EssayAnalysisService : IEssayAnalysisService
 {
-    public partial class EssayAnalysisService : IEssayAnalysisService
+    private readonly IOpenAiBroker openAiBroker;
+    private readonly ILoggingBroker loggingBroker;
+
+    public EssayAnalysisService(
+        IOpenAiBroker openAiBroker,
+        ILoggingBroker loggingBroker)
     {
-        private readonly IOpenAiBroker openAiBroker;
-        private readonly ILoggingBroker loggingBroker;
+        this.openAiBroker = openAiBroker;
+        this.loggingBroker = loggingBroker;
+    }
 
-        public EssayAnalysisService(
-            IOpenAiBroker openAiBroker,
-            ILoggingBroker loggingBroker)
+    public ValueTask<string> AnalyzeEssayAsync(string essay) =>
+        TryCatch(async () =>
         {
-            this.openAiBroker = openAiBroker;
-            this.loggingBroker = loggingBroker;
-        }
+            ValidateEssayAnalysisIsNotNull(essay);
 
-        public ValueTask<string> AnalyzeEssayAsync(string essay) =>
-            TryCatch(async () =>
-            {
-                ValidateEssayAnalysisIsNotNull(essay);
+            ChatCompletion request = CreateRequest(essay);
+            ChatCompletion response = 
+                await this.openAiBroker.EssayAnalysisAsync(request);
 
-                ChatCompletion request = CreateRequest(essay);
-                ChatCompletion response =
-            await this.openAiBroker.EssayAnalysisAsync(request);
+            return response.Response.Choices.FirstOrDefault().Message.Content;
+        });
 
-                return response.Response.Choices.FirstOrDefault().Message.Content;
-            });
-
-        public static ChatCompletion CreateRequest(string essay)
+    private static ChatCompletion CreateRequest(string essay)
+    {
+        return new ChatCompletion
         {
-            return new ChatCompletion
+            Request = new ChatCompletionRequest
             {
-                Request = new ChatCompletionRequest
+                Model = "gpt-4-1106-preview",
+                MaxTokens = 1500,
+                Messages = new ChatCompletionMessage[]
                 {
-                    Model = "gpt-4-1106-preview",
-                    MaxTokens = 1500,
-                    Messages = new ChatCompletionMessage[]
+                    new ChatCompletionMessage
                     {
-                        new ChatCompletionMessage
-                        {
-                            Content = "You are IELTS Writing examiner. Give detailed IELTS feedback based on marking criteria of IELTS",
-                            Role = "system",
-                        },
-                        new ChatCompletionMessage
-                        {
-                            Content = essay,
-                            Role = "user",
-                        }
+                        Content = "You are IELTS Writing examiner. Give detailed IELTS feedback based on marking criteria of IELTS",
+                        Role = "system",
                     },
-                }
-            };
-        }
+                    new ChatCompletionMessage
+                    {
+                        Content = essay,
+                        Role = "user",
+                    }
+                },
+            }
+        };
     }
 }
