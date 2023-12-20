2988299aa3285bb5641897f88e8fb86bea282677
A'zam Amonov <84405537+azam-amonov@users.noreply.github.com>
2023-12-20 14:39:22 +0500

    CODE RUB: x

--- EssayAnalyzer.Api/Services/Foundation/Essays/EssayService.Exceptions.cs
+++ EssayAnalyzer.Api/Services/Foundation/Essays/EssayService.Exceptions.cs
@@ -41,7 +41,6 @@ public partial class EssayService
           catch (NotFoundEssayException notFoundException)
           {
                throw CreateAdnLogValidationException(notFoundException);
-
           }
           catch (Exception exception)
           {
