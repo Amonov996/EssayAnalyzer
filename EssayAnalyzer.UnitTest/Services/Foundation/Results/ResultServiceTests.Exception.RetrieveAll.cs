564b2cec335f7d1d3d3a7de678a052fe9ca40e97
Ravshan9812 <anorboev.ravshan@gmail.com>
2023-12-20 18:40:08 +0500

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlErrorOccursAndLogItAsync -> PASS

---
+++ EssayAnalyzer.UnitTest/Services/Foundation/Results/ResultServiceTests.Exception.RetrieveAll.cs
@@ -0,0 +1,50 @@
+﻿using EssayAnalyzer.Api.Models.Foundation.Results.Exceptions;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Xunit;
+
+namespace EssayAnalyzer.UnitTest.Services.Foundation.Results;
+
+public partial class ResultServiceTests
+{
+    [Fact]
+    public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlErrorOccursAndLogItAsync()
+    {
+        // given
+        SqlException sqlException = GetSqlException();
+
+        var failedResultStorageException =
+            new FailedResultStorageException(sqlException);
+
+        var expectedResultDependencyException =
+            new ResultDependencyException(failedResultStorageException);
+
+        this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllResults())
+            .Throws(sqlException);
+
+        // when
+        Action retrieveAllResultsAction = () =>
+            this.resultService.RetrieveAllResults();
+
+        ResultDependencyException actualResultDependencyException =
+            Assert.Throws<ResultDependencyException>(retrieveAllResultsAction);
+
+        // then
+        actualResultDependencyException.Should().BeEquivalentTo(
+            expectedResultDependencyException);
+
+        this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllResults(),
+            Times.Once);
+
+        this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedResultDependencyException))),
+            Times.Once);
+
+        this.storageBrokerMock.VerifyNoOtherCalls();
+        this.loggingBrokerMock.VerifyNoOtherCalls();
+    }
+}
