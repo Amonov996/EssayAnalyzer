1b9e3a52fe6ae8b3707e7be586a618b6eaf14532
Ravshan9812 <anorboev.ravshan@gmail.com>
2023-12-20 12:54:01 +0500

    ShouldThrowServiceExceptionOnAddIfServiceErrorOccursAndLogItAsync -> FAIL

--- EssayAnalyzer.UnitTest/Services/Foundation/Results/ResultServiceTests.Exception.Add.cs
+++ EssayAnalyzer.UnitTest/Services/Foundation/Results/ResultServiceTests.Exception.Add.cs
@@ -44,4 +44,40 @@ public partial class ResultServiceTests
         this.loggingBrokerMock.VerifyNoOtherCalls();
         this.storageBrokerMock.VerifyNoOtherCalls();
     }
+
+    [Fact]
+    public async Task ShouldThrowServiceExceptionOnAddIfServiceErrorOccursAndLogItAsync()
+    {
+        // given
+        Result randomResult = CreateRandomResult();
+        Exception serviceException = new Exception();
+
+        var failedResultServiceException =
+            new FailedResultServiceException(serviceException);
+
+        var expectedResultServiceException =
+            new ResultServiceException(failedResultServiceException);
+
+        this.storageBrokerMock.Setup(broker =>
+            broker.InsertResultAsync(randomResult)).ThrowsAsync(serviceException);
+
+        // when
+        ValueTask<Result> addResultTask = this.resultService.AddResultAsync(randomResult);
+
+        var actuaResultServiceException =
+            await Assert.ThrowsAsync<ResultServiceException>(addResultTask.AsTask);
+
+        // then
+        actuaResultServiceException.Should().BeEquivalentTo(expectedResultServiceException);
+
+        this.loggingBrokerMock.Verify(broker =>
+            broker.LogError(It.Is(SameExceptionAs(expectedResultServiceException))),
+            Times.Once);
+
+        this.storageBrokerMock.Verify(broker =>
+            broker.InsertResultAsync(randomResult), Times.Once);
+
+        this.loggingBrokerMock.VerifyNoOtherCalls();
+        this.storageBrokerMock.VerifyNoOtherCalls();
+    }
 }
