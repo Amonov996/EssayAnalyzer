cbd55606331cb684946ea0360f40ed60f9e16d90
Ravshan9812 <anorboev.ravshan@gmail.com>
2023-12-21 12:26:10 +0500

    ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync -> PASS

---
+++ EssayAnalyzer.UnitTest/Services/Foundation/Results/ResultServiceTests.Validation.RetrieveById.cs
@@ -0,0 +1,45 @@
+﻿using EssayAnalyzer.Api.Models.Foundation.Results;
+using EssayAnalyzer.Api.Models.Foundation.Results.Exceptions;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace EssayAnalyzer.UnitTest.Services.Foundation.Results;
+
+public partial class ResultServiceTests
+{
+    [Fact]
+    public async Task ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync()
+    {
+        // given
+        Guid invalidResultId = Guid.Empty;
+        var invalidResultException = new InvalidResultException();
+
+        invalidResultException.AddData(
+            key: nameof(Result.Id),
+            values: "Id is required");
+
+        var expectedResultValidationException =
+            new ResultValidationException(invalidResultException);
+
+        // when
+        ValueTask<Result> retrieveResultByIdTask =
+            this.resultService.RetrieveResultByIdAsync(invalidResultId);
+
+        ResultValidationException actualResultValidationException =
+            await Assert.ThrowsAsync<ResultValidationException>(retrieveResultByIdTask.AsTask);
+
+        // then
+        actualResultValidationException.Should().BeEquivalentTo(expectedResultValidationException);
+
+        this.loggingBrokerMock.Verify(broker =>
+            broker.LogError(It.Is(SameExceptionAs(expectedResultValidationException))),
+            Times.Once);
+
+        this.storageBrokerMock.Verify(broker =>
+            broker.SelectResultByIdAsync(It.IsAny<Guid>()),Times.Never);
+
+        this.loggingBrokerMock.VerifyNoOtherCalls();
+        this.storageBrokerMock.VerifyNoOtherCalls();
+    }
+}
