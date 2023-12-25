7fc1d2738a2867a4d3383b8fa5a8e7848b0cda89
Ravshan9812 <anorboev.ravshan@gmail.com>
2023-12-25 12:10:11 +0500

    CONTROLLERS: result controller added

--- EssayAnalyzer.Api/Controllers/ResultController.cs
+++ EssayAnalyzer.Api/Controllers/ResultController.cs
@@ -1,7 +1,90 @@
+using EssayAnalyzer.Api.Models.Foundation.Results;
+using EssayAnalyzer.Api.Models.Foundation.Results.Exceptions;
+using EssayAnalyzer.Api.Services.Foundation.Results;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
 namespace EssayAnalyzer.Api.Controllers;
 
-
-public class ResultController
+[ApiController]
+[Route("api/[controller]")]
+public class ResultController : RESTFulController
 {
-    
+    private readonly IResultService resultService;
+
+    public ResultController(IResultService resultService) =>
+        this.resultService = resultService;
+
+    [HttpPost]
+    public async ValueTask<ActionResult<Result>> PostResultAsync(Result result)
+    {
+        try
+        {
+            Result postedResult = await this.resultService
+                .AddResultAsync(result);
+
+            return Created(postedResult);
+        }
+        catch (ResultValidationException resultValidationException)
+        {
+            return BadRequest(resultValidationException.InnerException);
+        }
+        catch (ResultDependencyException resultDependencyException)
+        {
+            return InternalServerError(resultDependencyException);
+        }
+        catch (ResultServiceException resultServiceException)
+        {
+            return InternalServerError(resultServiceException);
+        }
+    }
+
+    [HttpGet]
+    public ActionResult<IQueryable<Result>> GetAllResults()
+    {
+        try
+        {
+            IQueryable<Result> results = this.resultService
+                .RetrieveAllResults();
+
+            return Ok(results);
+        }
+        catch (ResultDependencyException resultDependencyException)
+        {
+            return InternalServerError(resultDependencyException);
+        }
+        catch (ResultServiceException resultServiceException)
+        {
+            return InternalServerError(resultServiceException);
+        }
+    }
+
+    [HttpGet("{id}")]
+    public async ValueTask<ActionResult<Result>> GetResultByIdAsync(Guid id)
+    {
+        try
+        {
+            Result result = await this.resultService
+                .RetrieveResultByIdAsync(id);
+
+            return Ok(result);
+        }
+        catch (ResultValidationException resultValidationException)
+            when(resultValidationException.InnerException is NotFoundResultException)
+        {
+            return NotFound(resultValidationException.InnerException);
+        }
+        catch (ResultValidationException resultValidationException)
+        {
+            return BadRequest(resultValidationException.InnerException);
+        }
+        catch (ResultDependencyException resultDependencyException)
+        {
+            return InternalServerError(resultDependencyException);
+        }
+        catch (ResultServiceException resultServiceException)
+        {
+            return InternalServerError(resultServiceException);
+        }
+    }
 }
