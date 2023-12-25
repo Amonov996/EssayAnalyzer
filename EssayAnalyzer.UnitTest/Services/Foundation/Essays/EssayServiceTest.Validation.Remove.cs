bc90beec031daf20a8fff51d9ea64f368393d422
Ravshan9812 <anorboev.ravshan@gmail.com>
2023-12-25 17:52:01 +0500

    ShouldThrowValidationExceptionOnRemoveIfIdIsInvalidAndLogItAsync -> PASS

---
+++ EssayAnalyzer.UnitTest/Services/Foundation/Essays/EssayServiceTest.Validation.Remove.cs
@@ -0,0 +1,49 @@
+﻿using EssayAnalyzer.Api.Models.Foundation.Essays;
+using EssayAnalyzer.Api.Models.Foundation.Essays.Exceptions;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace EssayAnalyzer.UnitTest.Services.Foundation.Essays;
+
+public partial class EssayServiceTest
+{
+    [Fact]
+    public async Task ShouldThrowValidationExceptionOnRemoveIfIdIsInvalidAndLogItAsync()
+    {
+        // given
+        Guid invalidId = Guid.Empty;
+
+        var invalidEssayException =
+            new InvalidEssayException();
+
+        invalidEssayException.AddData(
+            key: nameof(Essay.Id),
+            values: "Id is required");
+
+        var expectedEssayValidationException =
+            new EssayValidationException(invalidEssayException);
+
+        // when
+        ValueTask<Essay> removeEssayTask = this.essayService.RemoveEssayByIdAsync(invalidId);
+
+        EssayValidationException actualEssayValidationException =
+            await Assert.ThrowsAsync<EssayValidationException>(removeEssayTask.AsTask);
+
+        // then
+        actualEssayValidationException.Should().BeEquivalentTo(expectedEssayValidationException);
+
+        this.loggingBrokerMock.Verify(broker =>
+            broker.LogError(It.Is(SameExceptionAs(expectedEssayValidationException))),
+            Times.Once);
+
+        this.storageBrokerMock.Verify(broker =>
+            broker.SelectEssayByIdAsync(It.IsAny<Guid>()), Times.Never);
+
+        this.storageBrokerMock.Verify(broker =>
+            broker.DeleteEssayAsync(It.IsAny<Essay>()), Times.Never);
+
+        this.loggingBrokerMock.VerifyNoOtherCalls();
+        this.storageBrokerMock.VerifyNoOtherCalls();
+    }
+}
