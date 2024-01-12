536b22044c4d1c0f5b860ecb5983d93fbc9a37d8
Azam Amonov <azamamonov555@gmail.com>
2024-01-12 20:56:50 +0500

    BROKERS: Add Telegram Bot Broker

---
+++ EssayAnalyzer.Api/Brokers/Telegram/TelegramBroker.cs
@@ -0,0 +1,25 @@
+using Telegram.Bot;
+
+namespace EssayAnalyzer.Api.Brokers.Telegram;
+
+public class TelegramBroker : ITelegramBroker
+{
+    private readonly IConfiguration configuration;
+
+    public TelegramBroker(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+        this.TelegramBotClient = ConfigureTelegramBotClient();
+        this.CancellationTokenSource = new CancellationTokenSource(); 
+    }
+
+    private ITelegramBotClient ConfigureTelegramBotClient()
+    {
+        var botToken = this.configuration.GetValue<string>(key: "TelegramBotToken");
+        return new TelegramBotClient(token: botToken);
+    }
+
+    public ITelegramBotClient TelegramBotClient { get; }
+    public CancellationTokenSource CancellationTokenSource { get; }
+}
+
